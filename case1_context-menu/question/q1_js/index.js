e5a09fbe030a61b8f8e2e532b80b8a10d8308345
seolhyelin <tosiltosilkk@gmail.com>
2022-04-29 00:27:09 +0900

    Add: Js toggle 기능

--- case1_context-menu/question/q1_js/index.js
+++ case1_context-menu/question/q1_js/index.js
@@ -1,1 +1,23 @@
 // Write Javascript code here!
+import "./style.css";
+
+const wrapper = document.querySelector(".wrapper");
+const items = document.querySelectorAll(".item");
+
+wrapper.addEventListener("click", function (e) {
+  const targetElem = e.target;
+  if (!targetElem.classList.contains("item")) return;
+  targetElem.classList.toggle("open");
+  items.forEach(function (elem) {
+    if (elem !== targetElem) elem.classList.remove("open");
+  });
+});
+
+// items.forEach(function (item) {
+//   item.addEventListener("click", function (e) {
+//     item.classList.toggle("open");
+//     items.forEach(function (elem) {
+//       if (elem !== item) elem.classList.remove("open");
+//     });
+//   });
+// });
